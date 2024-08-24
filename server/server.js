6470190dd2c1da8d1dfd9f9470b1faa82a8f5394
Rehman Khan <rehman.khan.300a@gmail.com>
2024-08-24 19:57:03 +0530

    bug fix for dist folder serving

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.use(cors());
 
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../client/dist' +'/index.html'));
+  res.sendFile(path.join(__dirname + '../client/dist' +'index.html'));
 });
 
 
